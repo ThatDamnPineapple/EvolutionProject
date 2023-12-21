c30ffd2ddc317a8d3f08340c02d2f575bae875a9
GraydeeSi <wiseguy0002@gmail.com>
2023-12-21 12:45:59 -0500

    Seasons

---
+++ ProjectContent/SceneStuff/SeasonManager.cs
@@ -0,0 +1,40 @@
+﻿using EvoSim.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvoSim.ProjectContent.SceneStuff
+{
+    public class SeasonManager : IUpdate, ILoadable
+    {
+        public static readonly float YEARLENGTH = 30;
+
+        public static readonly float BASESUNLIGHT = 2000;
+
+        public static readonly float SUNLIGHTVARIATION = 1500;
+
+        public static float SeasonSin => MathF.Sin((SeasonTimer * 6.28f) / YEARLENGTH);
+        public static float SeasonCos => MathF.Cos((SeasonTimer * 6.28f) / YEARLENGTH);
+
+        public static float CurrentRegen => BASESUNLIGHT + (SUNLIGHTVARIATION * SeasonSin);
+
+        public static float SeasonTimer;
+
+        public void Update(GameTime gameTime)
+        {
+            SeasonTimer += Main.delta;
+        }
+
+        public void Load()
+        {
+            Main.updatables.Add(this);
+        }
+
+        public void Unload()
+        {
+
+        }
+    }
+}
--- ProjectContent/Terrain/TerrainTypes/GrassSquare.cs
+++ ProjectContent/Terrain/TerrainTypes/GrassSquare.cs
@@ -10,8 +10,35 @@ namespace EvoSim.ProjectContent.Terrain.TerrainTypes
     internal class GrassSquare : TerrainSquare
     {
         public override int ID => 3;
-        public override Color color => Color.Green;
+        public override Color color => GetColor();
 
         public GrassSquare(Vector2 position, float width, float height) : base(position, width, height) { }
+
+        public static Color GetColor()
+        {
+            Color spring = new Color(49, 206, 77);
+            Color summer = new Color(199, 255, 82);
+            Color fall = new Color(255, 152, 62);
+            Color winter = new Color(171, 238, 255);
+            float sin = SeasonManager.SeasonSin;
+            float cos = SeasonManager.SeasonCos;
+            if (MathF.Sign(sin) == 1 && MathF.Sign(cos) == 1) //Spring => summer
+            {
+                return Color.Lerp(spring, summer, sin);
+            }
+            if (MathF.Sign(sin) == 1 && MathF.Sign(cos) != 1) //Summer => fall
+            {
+                return Color.Lerp(fall, summer, sin);
+            }
+            if (MathF.Sign(sin) != 1 && MathF.Sign(cos) != 1) //fall => winter
+            {
+                return Color.Lerp(fall, winter, -sin);
+            }
+            if (MathF.Sign(sin) != 1 && MathF.Sign(cos) == 1) //winter => spring
+            {
+                return Color.Lerp(winter, spring, cos);
+            }
+            return Color.White;
+        }
     }
 }
